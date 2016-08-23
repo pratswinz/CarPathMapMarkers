b9c1974e712f1dcbf0c563f241c6ec46c8fb8210
pratswinz <prateekonline3030@gmail.com>
2016-08-24 03:00:44 +0530

    Add files via upload

--- app/config.js
+++ app/config.js
@@ -1,1 +1,12 @@
+// Sets the MongoDB Database options
 
+module.exports = {
+
+    localtest:
+    {
+        name: "scotch-user-map-local",
+        url: "mongodb://localhost/IntelliCar",
+        port: 27017
+    }
+
+};
